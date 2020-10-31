0f113a2027a0bb49224ae57d0801765d42a005f2
Matthew Penner <me@matthewp.io>
2020-10-31 17:47:41 -0600

    Switch to s3 multipart uploads for backups

--- api/backup_endpoints.go
+++ api/backup_endpoints.go
@@ -1,10 +1,35 @@
 package api
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/pkg/errors"
 )
+
+type BackupRemoteUploadResponse struct {
+	CompleteMultipartUpload string
+	AbortMultipartUpload    string
+	Parts                   []string
+	PartSize                int64
+}
+
+func (r *Request) GetBackupRemoteUploadURLs(backup string, size int64) (*BackupRemoteUploadResponse, error) {
+	resp, err := r.Get(fmt.Sprintf("/backups/%s?size=%d", backup, size), nil)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.HasError() {
+		return nil, resp.Error()
+	}
+
+	var res BackupRemoteUploadResponse
+	if err := resp.Bind(&res); err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return &res, nil
+}
 
 type BackupRequest struct {
 	Checksum     string `json:"checksum"`
@@ -16,16 +41,15 @@ type BackupRequest struct {
 // Notifies the panel that a specific backup has been completed and is now
 // available for a user to view and download.
 func (r *Request) SendBackupStatus(backup string, data BackupRequest) error {
-	b, err := json.Marshal(data)
+	resp, err := r.Post(fmt.Sprintf("/backups/%s", backup), data)
 	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	resp, err := r.Post(fmt.Sprintf("/backups/%s", backup), b)
-	if err != nil {
-		return errors.WithStack(err)
+		return err
 	}
 	defer resp.Body.Close()
 
-	return resp.Error()
+	if resp.HasError() {
+		return resp.Error()
+	}
+
+	return nil
 }
